f65ac81b16300565181b9a19e6825677740efc1e
CallMeSevill <rainielsevilla200@gmail.com>
2024-10-18 12:46:13 +0800

    change the text colo into red in the nav bar

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 export default function Home() {
 	return (
-		<>
-			<h1>Hello Mundo!</h1>
+		<>	
+ <nav>
+      <a href="#">Home</a> |
+      <a href="#">Works</a> |
+      <a href="#">Contact</a> |
+      <a href="#">About</a> |   
+ </nav>
 		</>
 	)
 }
