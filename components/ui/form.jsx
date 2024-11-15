278b64c38a4e74ce23441eb42a723b080f93341c
bryan308 <cdrcangulo@gmail.com>
2024-11-15 20:59:34 +0800

    feat: Implement ContactPage with a new form component and refactor layout using Section

---
+++ components/ui/form.jsx
@@ -0,0 +1,59 @@
+import Link from "next/link"
+import { Textarea } from "@/components/ui/textarea"
+
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function Form() {
+	return (
+		<Card className="mt-12 mx-auto max-w-md bg-transparent">
+			{/* <CardHeader>
+				<CardTitle className="text-2xl">Login</CardTitle>
+				<CardDescription>Enter your email below to login to your account</CardDescription>
+			</CardHeader> */}
+			<CardContent className="pt-6">
+				<div className="grid gap-4">
+					<div className="grid gap-2">
+						<Label htmlFor="fname">First Name</Label>
+						<Input
+							id="fname"
+							type="text"
+							required
+						/>
+					</div>
+					<div className="grid gap-2">
+						<Label htmlFor="lname">Last Name</Label>
+						<Input
+							id="lname"
+							type="text"
+							required
+						/>
+					</div>
+					<div className="grid gap-2">
+						<Label htmlFor="email">Email</Label>
+						<Input
+							id="email"
+							type="text"
+							required
+						/>
+					</div>
+					<div className="grid gap-2">
+						<Label htmlFor="message">Feedback or Message</Label>
+						<Textarea
+							id="message"
+							placeholder="Enter you message here"
+						/>
+					</div>
+					<Button
+						type="submit"
+						className="w-full"
+					>
+						Submit
+					</Button>
+				</div>
+			</CardContent>
+		</Card>
+	)
+}
